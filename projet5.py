90bdb4fdbec61cebb3b4a455169269533f26e11e
Laure <lauremenaut@yahoo.fr>
2018-03-09 22:36:25 -0500

    Improved 'INSERT IGNORE INTO Product_Categorie' query (now working) - Moved up 'except KeyError - Modified mysql password'

--- projet5.py
+++ projet5.py
@@ -8,20 +8,23 @@ criteria = {
             "action" : "process",
             "json" : 1,
             "countries" : "France",
-            "page_size" : 20,
-            "page" : 6,
+            "page_size" : 100,
+            "page" : 1,
             "tagtype_0" : "categories",
             "tag_contains_0" : "contains",
-            "tag_0" : "sodas"
+            "tag_0" : "boissons",
+            "tagtype_1" : "nutrition_grades",
+            "tag_contains_1" : "contains",
+            "tag_1" : "e"
             }
 
 
-r = requests.get('https://world.openfoodfacts.org/cgi/search.pl', params=criteria)
+r = requests.get('https://fr.openfoodfacts.org/cgi/search.pl', params=criteria)
 
 data = r.json()
 products = data["products"] # products est une liste de dictionnaires correspondant aux produits de la page 1
 
-db = records.Database('mysql+pymysql://lauredougui:crampagna09@localhost/healthier_food?charset=utf8')
+db = records.Database('mysql+pymysql://lauredougui:mysql@localhost/healthier_food?charset=utf8')
 
 for product in products:
     try:
@@ -36,20 +39,23 @@ for product in products:
         nutrition_grade = product["nutrition_grades"]
 
         if categories and name and description and brand and url and store and nutrition_grade:
-            db.query('INSERT IGNORE INTO Product (name, description, brand, url, nutriscore) \
-                VALUES (:name, :description, :brand, :url, :nutrition_grade)', \
-                name=name, description=description, brand=brand, url=url, nutrition_grade=nutrition_grade \
+            db.query("""INSERT IGNORE INTO Product (name, description, brand, url, nutriscore)
+                VALUES (:name, :description, :brand, :url, :nutrition_grade)""",
+                name=name, description=description, brand=brand, url=url, nutrition_grade=nutrition_grade
                 )
+
             db.query('INSERT IGNORE INTO Store (name) VALUES (:store)', store=store)
 
             for categorie in categories.split(","):
                 db.query('INSERT IGNORE INTO Categorie (name) VALUES (:categorie)', categorie=categorie)
 
-            db.query('INSERT IGNORE INTO Product_Categorie (product_id) \
-                SELECT product_id FROM Product WHERE name = :name', name=name)
-            db.query('UPDATE Product SET store_id = \
-                (SELECT store_id FROM Store WHERE name = :store) \
-                WHERE name = :name', store=store, name=name)
+                db.query("""INSERT IGNORE INTO Product_Categorie (product_id, categorie_id) VALUES
+                    ((SELECT product_id FROM Product WHERE name = :name),
+                    (SELECT categorie_id FROM Categorie WHERE name = :categorie))""", name=name, categorie=categorie)
+
+            db.query("""UPDATE Product SET store_id =
+                (SELECT store_id FROM Store WHERE name = :store)
+                WHERE name = :name""", store=store, name=name)
 
     except KeyError:
         print("Missing data")
