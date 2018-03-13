e6ed8d80648b19d2151d21d0bed75bd62ed8fb8e
Laure <lauremenaut@yahoo.fr>
2018-03-12 23:52:38 -0400

    Created tags.py file containing 'categories' and 'nutrition_grades' tags

---
+++ tags.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env phyton3
+# coding: utf-8
+
+""" Contains nutrition_grades and categories tags. """
+
+nutrition_grades = ["a", "b", "d", "e"]
+
+categories = ["Boissons sucrées", "Pains", "Biscuits", "Desserts", "Céréales au chocolat"]
