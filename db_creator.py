976888ee3bafdd3c9c35794e3a8199f051b2dbbc
Laure <lauremenaut@yahoo.fr>
2018-03-14 13:20:11 -0400

    Added create_database(), create_tables() and create_foreign_keys() methods - Import database (from config)

--- db_creator.py
+++ db_creator.py
@@ -7,25 +7,28 @@ Creator class creates a new database called 'healthier_food'.
 
 """
 
-import records
+from config import database
 
 
 class DB_Creator:
     """ Sets DB_Creator class """
     def __init__(self):
         """ DB_Creator constructor """
+        self.create_database()
+        self.create_tables()
+        self.create_foreign_keys()
 
         #  La base doit déjà exister pour pouvoir s'y connecter ... un peu paradoxal pour une classe de création :-/
 
-        #  Est-ce que je laisse tout dans la méthode __init__ ... ?
+    def create_database(self):
+        """ Manages database creation """
+        database.query('DROP DATABASE IF EXISTS healthier_food')
+        database.query('CREATE DATABASE healthier_food CHARACTER SET "utf8"')
+        database.query('USE healthier_food')
 
-        self.db = records.Database('mysql+pymysql://lauredougui:mysql@localhost/healthier_food?charset=utf8')
-
-        self.db.query('DROP DATABASE IF EXISTS healthier_food')
-        self.db.query('CREATE DATABASE healthier_food CHARACTER SET "utf8"')
-        self.db.query('USE healthier_food')
-
-        self.db.query('''CREATE TABLE Product (
+    def create_tables(self):
+        """ Manages tables creation """
+        database.query('''CREATE TABLE Product (
                         product_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
                         name VARCHAR(100) UNIQUE NOT NULL,
                         description VARCHAR(100) NOT NULL,
@@ -41,22 +44,22 @@ class DB_Creator:
                         labels VARCHAR(150),
                         PRIMARY KEY (product_id))''')
 
-        self.db.query('''CREATE TABLE Store (
+        database.query('''CREATE TABLE Store (
                         store_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
                         name VARCHAR(50) UNIQUE NOT NULL,
                         PRIMARY KEY (store_id))''')
 
-        self.db.query('''CREATE TABLE Categorie (
+        database.query('''CREATE TABLE Categorie (
                         categorie_id SMALLINT UNSIGNED AUTO_INCREMENT NOT NULL,
                         name VARCHAR(150) UNIQUE NOT NULL,
                         PRIMARY KEY (categorie_id))''')
 
-        self.db.query('''CREATE TABLE Product_Categorie (
+        database.query('''CREATE TABLE Product_Categorie (
                         product_id INT UNSIGNED NOT NULL,
                         categorie_id SMALLINT UNSIGNED NOT NULL,
                         PRIMARY KEY (product_id, categorie_id))''')
 
-        self.db.query('''CREATE TABLE History (
+        database.query('''CREATE TABLE History (
                         history_id INT UNSIGNED AUTO_INCREMENT NOT NULL,
                         request_date DATETIME,
                         unhealthy_product VARCHAR(100),
@@ -66,19 +69,21 @@ class DB_Creator:
                         url VARCHAR(150),
                         PRIMARY KEY (history_id))''')
 
-        self.db.query('''ALTER TABLE Product ADD CONSTRAINT store_product_fk
+    def create_foreign_keys(self):
+        """ Manages foreign keys creation """
+        database.query('''ALTER TABLE Product ADD CONSTRAINT store_product_fk
         FOREIGN KEY (store_id)
         REFERENCES Store (store_id)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION''')
 
-        self.db.query('''ALTER TABLE Product_Categorie ADD CONSTRAINT product_product_categorie_fk
+        database.query('''ALTER TABLE Product_Categorie ADD CONSTRAINT product_product_categorie_fk
         FOREIGN KEY (product_id)
         REFERENCES Product (product_id)
         ON DELETE NO ACTION
         ON UPDATE NO ACTION''')
 
-        self.db.query('''ALTER TABLE Product_Categorie ADD CONSTRAINT categorie_product_categorie_fk
+        database.query('''ALTER TABLE Product_Categorie ADD CONSTRAINT categorie_product_categorie_fk
         FOREIGN KEY (categorie_id)
         REFERENCES Categorie (categorie_id)
         ON DELETE NO ACTION
@@ -86,7 +91,7 @@ class DB_Creator:
 
 
 def main():
-    healthier_food = DB_Creator()
+    DB_Creator()
 
 
 if __name__ == "__main__":
