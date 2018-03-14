c2a962d229a5cc5e10fe69eff46f9c04a38ffb4e
Laure <lauremenaut@yahoo.fr>
2018-03-13 23:37:34 -0400

    Improved __init__() method - Added save_result() & get_saved_results() methods - Added 'udpate' argument with argparse (and import db_creator, db_updater)

--- app.py
+++ app.py
@@ -3,26 +3,64 @@
 
 """ Sets App class.
 
-App class manages interactions with the user """
+App class manages interactions with the user.
+
+"""
+
+import argparse
 
 import records
 
+import db_creator
+import db_updater
 import tags
 
 
 class App:
     """ Sets App class """
-    def __init__(self):
+    def __init__(self, update):
         """ App constructor """
-        self.choose_categorie()
-        self.choose_unhealthy_product()
-        self.get_healthy_product()
+
+        if update:
+            healthier_food = db_creator.DB_Creator()
+            healthier_food = db_updater.DB_Updater()
+
+        self.database = records.Database('mysql+pymysql://lauredougui:mysql@localhost/healthier_food?charset=utf8')
+        carry_on = True
+        print("\nBonjour, que souhaitez-vous faire ?")
+
+        while carry_on:
+            print("\n1 - Chercher une alternative plus saine à un nouvel aliment")
+            print("2 - Retrouver vos substitutions enregistrées")
+            print("3 - Quitter l'application")
+
+            try:
+                starting_choice = int(input())
+                assert starting_choice in [1, 2, 3]
+            except ValueError:
+                print("\nVeuillez saisir un nombre : 1, 2 ou 3.")
+                continue
+            except AssertionError:
+                print("\n{} est un choix invalide. Veuillez saisir 1, 2 ou 3.".format(starting_choice))
+                continue
+            else:
+                if starting_choice == 1:
+                    self.choose_categorie()
+                    self.choose_unhealthy_product()
+                    self.get_healthy_product()
+                    self.save_result()
+                elif starting_choice == 2:
+                    self.get_saved_results()
+                elif starting_choice == 3:
+                    print("\nMerci de votre visite !\n")
+                    carry_on = False
 
     def choose_categorie(self):
-        print("Veuillez saisir le numéro d'une categorie : ")
+        """ Manages categorie selection """
+        print("\nVeuillez saisir le numéro d'une categorie :\n")
         for categorie in tags.categories:
             position = tags.categories.index(categorie) + 1
-            print(position, categorie)
+            print("{} - {}".format(position, categorie))
 
         try:
             categorie_choice = int(input())
@@ -35,20 +73,24 @@ class App:
             self.choose_categorie()
 
     def choose_unhealthy_product(self):
-        self.database = records.Database('mysql+pymysql://lauredougui:mysql@localhost/healthier_food?charset=utf8')
+        """ Manages product selection """
+        unhealthy_products = self.database.query("""SELECT Product.name
+            FROM Product
+            JOIN Product_Categorie
+            ON Product.product_id = Product_Categorie.product_id
+            JOIN Categorie
+            ON Categorie.categorie_id = Product_Categorie.categorie_id
+            WHERE Categorie.name = :selected_categorie
+            AND (Product.nutriscore = "e" OR Product.nutriscore = "d")
+            """, selected_categorie=self.selected_categorie)
 
-        unhealthy_products = self.database.query("""SELECT Product.name FROM Product
-            JOIN Product_Categorie ON Product.product_id = Product_Categorie.product_id
-            JOIN Categorie ON Categorie.categorie_id = Product_Categorie.categorie_id
-            WHERE Categorie.name = :selected_categorie AND (Product.nutriscore = "e" OR Product.nutriscore = "d") """, selected_categorie=self.selected_categorie)
-
-        print("Veuillez maintenant saisir le numéro d'un produit de la categorie {} :".format(self.selected_categorie))
+        print("\nVeuillez maintenant saisir le numéro d'un produit de la categorie {} :\n".format(self.selected_categorie))
 
         # Comment accéder au nombre de produits ? L'objet "products" n'a pas de longueur ...
 
         for i in range(10):
             try:
-                print(i + 1, unhealthy_products[i]["name"])
+                print("{} - {}".format(i + 1, unhealthy_products[i]["name"].capitalize()))
                 i += 1
             except IndexError:
                 pass
@@ -64,17 +106,106 @@ class App:
             self.choose_unhealthy_product()
 
     def get_healthy_product(self):
-        #  Work in progress
-        healthy_product = self.database.query("""SELECT Product.name
+        """ Returns an healthy product and its details """
+        #  Work in progress : ajouter la correspondance de plusieurs catégories pour améliorer la pertinence du résultat
+        self.healthy_products = self.database.query("""SELECT Product.product_id, Product.name, Product.description, Product.store_id, Product.url
             FROM Product
-            WHERE (Product.nutriscore = "a" OR Product.nutriscore = "b")""")
+            JOIN Product_Categorie
+            ON Product.product_id = Product_Categorie.product_id
+            JOIN Categorie
+            ON Categorie.categorie_id = Product_Categorie.categorie_id
+            WHERE (Product.nutriscore = "a" OR Product.nutriscore = "b")
+            AND Categorie.name = :selected_categorie
+            """, selected_categorie=self.selected_categorie)
 
-        print("'{}' pourrait être une alternative plus saine à '{}'.".format(healthy_product[0]["name"], self.selected_unhealthy_product))
+        for i in range(2):  # A supprimer
+            print(self.healthy_products[i]["product_id"])  # A supprimer
 
+        print("\nVoici une alternative plus saine à '{}' :\n".format(self.selected_unhealthy_product))
+        print("{}".format(self.healthy_products[0]["name"]).capitalize())
+        print("{}".format(self.healthy_products[0]["description"]).capitalize())
+        print("Disponible chez {}".format(self.healthy_products[0]["store_id"]).capitalize())  # Remplacer l'id par le nom du magasin
+        print("{}".format(self.healthy_products[0]["url"]))
+        # Ajoute-t-on d'autres informations ?
+
+        #  Faut-il ajouter un 'return' ?
+
+    def save_result(self):
+        """ Manages result backup """
+        print("\nSouhaitez-vous enregistrer ce résultat pour le retrouver plus tard ?\n")
+        print("1 - Oui, je sauvegarde")
+        print("2 - Non, merci")
+
+        try:
+            backup_choice = int(input())
+            assert backup_choice in [1, 2]
+        except ValueError:
+            print("\nVeuillez saisir un nombre : 1 ou 2.")
+            self.save_result()
+        except AssertionError:
+            print("\n{} est un choix invalide. Veuillez saisir 1 ou 2.".format(backup_choice))
+            self.save_result()
+        else:
+            if backup_choice == 1:
+                self.database.query("""INSERT INTO History
+                    VALUES (NULL, NOW(), :unhealthy_product, :healthy_product, :description, :store, :url)""",
+                                    unhealthy_product=self.selected_unhealthy_product,
+                                    healthy_product=self.healthy_products[0]["name"],
+                                    description=self.healthy_products[0]["description"],
+                                    store=self.healthy_products[0]["store_id"],
+                                    url=self.healthy_products[0]["url"])
+                print("\nRésultat sauvegardé !")
+            elif backup_choice == 2:
+                pass
+        finally:
+            print("\nQue souhaitez-vous faire maintenant ?")
+            # Attention : autant de print que d'exceptions levées ...
+
+    def get_saved_results(self):
+        """ Manages """
+        saved_results = self.database.query("""SELECT *
+            FROM History
+            ORDER BY request_date DESC
+            LIMIT 20""")
+
+        print("\nVoici les résultats de vos dernières recherches :")
+
+        for saved_result in saved_results:
+            print("\n{}".format(saved_result["request_date"]))
+            print("Produit à substituer : '{}'".format(saved_result["unhealthy_product"]))
+            print("Produit proposé : '{}'".format(saved_result["healthy_product"]))
+            print("{}".format(saved_result["description"]))
+            print("Disponible chez {}".format(saved_result["store"]))
+            print("{}".format(saved_result["url"]))
+
+
+def parse_arguments():
+    """ Returns an arguments parser with an "update" argument. """
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--update", action="store_true",
+        help="database update")
+    return parser.parse_args()
 
 def main():
-    app = App()
+    argument = parse_arguments()
+    app = App(argument.update)
 
 
 if __name__ == "__main__":
     main()
+
+
+"""
+        healthy_product = self.database.query('''SELECT Product.name
+            FROM (SELECT Product.name
+            FROM Product
+            JOIN Product_Categorie
+            ON Product.product_id = Product_Categorie.product_id
+            JOIN Categorie
+            ON Categorie.categorie_id = Product_Categorie.categorie_id
+            WHERE (Product.nutriscore = "a" OR Product.nutriscore = "b")
+            AND Categorie.name = :selected_categorie)
+            WHERE
+            ''')
+
+"""
