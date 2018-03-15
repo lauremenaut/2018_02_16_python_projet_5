276be8f846341795cc1ea4c32977a5278c69b68b
Laure <lauremenaut@yahoo.fr>
2018-03-15 15:20:52 -0400

    Added 'db_name' variable

--- config.py
+++ config.py
@@ -3,10 +3,18 @@
 
 """ Contains configuration data """
 
-import records
+from records import Database
+import datetime
 
 # les identifiants devraient être dans un fichier séparé, ignoré par Git
-database = records.Database('mysql+pymysql://lauredougui:mysql@localhost/healthier_food?charset=utf8')
+mysql_id = "lauredougui"
+mysql_pw = "mysql"
+
+# db_name = "healthier_food_" + str(datetime.datetime.now()) Problème de droits d'accès à la base !
+db_name = "healthier_food"
+
+# database = Database(f'mysql+pymysql://{mysql_id}:{mysql_pw}@localhost')
+database = Database(f'mysql+pymysql://{mysql_id}:{mysql_pw}@localhost/{db_name}?charset=utf8')
 
 nutrition_grades = ["a", "b", "d", "e"]
 
