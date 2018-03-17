98e622cd7c93b85d593b25cb573489304e5821a4
Laure <lauremenaut@yahoo.fr>
2018-03-17 00:15:58 -0400

    Import db_name - Added all() method in 'if' structure - Added 'for store in stores' queries loop

--- db_updater.py
+++ db_updater.py
@@ -3,20 +3,22 @@
 
 """ Sets DB_Updater class.
 
-DB_Updater class fills "healthier_food" database, connecting with Open Food Facts API.
+DB_Updater class fills "healthier_food" database, connecting with Open
+Food Facts API.
 
 """
 
 from requests import get
 
-from config import database, db_name, nutrition_grades, categories
+from config import database, db_name, nutrition_grades, tag_categories
+
 
 class DB_Updater:
     """ Sets DB_Updater class """
     def __init__(self):
         """ DB_Updater constructor """
         for self.nutrition_grade in nutrition_grades:
-            for self.categorie in categories:
+            for self.categorie in tag_categories:
                 self.products = self.get_products()
                 self.fill_db(self.products)
 
@@ -36,9 +38,12 @@ class DB_Updater:
             "tag_1": self.nutrition_grade
             }
 
-        response = get('https://fr.openfoodfacts.org/cgi/search.pl', params=criteria)
+        response = get('https://fr.openfoodfacts.org/cgi/search.pl',
+                       params=criteria)
         data = response.json()
-        products = data["products"]  # products est une liste de dictionnaires correspondant aux produits de la page 1
+        products = data["products"]
+        # 'products' est une liste de dictionnaires correspondant aux
+        # produits de la page 1
         return products
 
     def fill_db(self, products):
@@ -47,41 +52,60 @@ class DB_Updater:
         # database.query(f'USE {db_name}')
         for product in products:
             try:
-                categories = product["categories"]
+                categories = product["categories"].split(",")
                 name = product["product_name"]
                 description = product["generic_name"]
                 brands = (product["brands"]).split(",")
                 brand = brands[0]
                 url = product["url"]
                 stores = (product["stores"]).split(",")
-                store = stores[0]
                 nutrition_grade = product["nutrition_grades"]
-                allergens = product["allergens"]
-                traces = product["traces"]
-                labels = product["labels"]
 
             except KeyError:
                 print("Missing data")
 
-            if all([categories, name, description, brand, url, store, nutrition_grade]):
-                database.query("""INSERT IGNORE INTO Product (name, description, brand, url, nutriscore, allergens, traces, labels)
-                    VALUES (:name, :description, :brand, :url, :nutrition_grade, :allergens, :traces, :labels)""",
-                               name=name, description=description, brand=brand, url=url, nutrition_grade=nutrition_grade, allergens=allergens, traces=traces, labels=labels)
+            if all([categories, name, description, brand, url, stores,
+                    nutrition_grade]):
+                database.query("""INSERT IGNORE INTO Product (name,
+                                                              description,
+                                                              brand,
+                                                              url,
+                                                              nutriscore)
+                                VALUES (:name,
+                                        :description,
+                                        :brand,
+                                        :url,
+                                        :nutrition_grade)""",
+                               name=name,
+                               description=description,
+                               brand=brand,
+                               url=url,
+                               nutrition_grade=nutrition_grade)
 
-                database.query('INSERT IGNORE INTO Store (name) VALUES (:store)', store=store)
+                for categorie in categories:
+                    database.query('''INSERT IGNORE INTO Categorie (name)
+                                   VALUES (:categorie)''',
+                                   categorie=categorie)
 
-                for categorie in categories.split(","):
-                    database.query('INSERT IGNORE INTO Categorie (name) VALUES (:categorie)', categorie=categorie)
+                    database.query('''INSERT IGNORE INTO
+                                   Product_Categorie (product_id, categorie_id)
+                                   VALUES ((SELECT product_id FROM Product
+                                            WHERE name = :name),
+                                           (SELECT categorie_id FROM Categorie
+                                            WHERE name = :categorie))''',
+                                   name=name, categorie=categorie)
 
-                    database.query("""INSERT IGNORE INTO Product_Categorie (product_id, categorie_id) VALUES
-                        ((SELECT product_id FROM Product WHERE name = :name),
-                        (SELECT categorie_id FROM Categorie WHERE name = :categorie))""", name=name, categorie=categorie)
+                for store in stores:
+                    database.query('''INSERT IGNORE INTO Store (name)
+                                   VALUES (:store)''', store=store)
 
-                database.query("""UPDATE Product SET store_id =
-                    (SELECT store_id FROM Store WHERE name = :store)
-                    WHERE name = :name""", store=store, name=name)
-
-        print(len(self.products))
+                    database.query('''INSERT IGNORE INTO
+                                   Product_Store (product_id, store_id)
+                                   VALUES ((SELECT product_id FROM Product
+                                            WHERE name = :name),
+                                           (SELECT store_id FROM Store
+                                            WHERE name = :store))''',
+                                   name=name, store=store)
 
 
 def main():
