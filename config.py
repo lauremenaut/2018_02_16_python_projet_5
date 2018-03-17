eb3282e98a100d8a4b518c60adc49ef1ffdbdacc
Laure <lauremenaut@yahoo.fr>
2018-03-17 00:09:27 -0400

    Exported id & pw - Added new tag_categories

--- config.py
+++ config.py
@@ -4,18 +4,21 @@
 """ Contains configuration data """
 
 from records import Database
+
 import datetime
 
-# les identifiants devraient être dans un fichier séparé, ignoré par Git
-mysql_id = "lauredougui"
-mysql_pw = "mysql"
+from untracked_config import mysql_id, mysql_pw
 
-# db_name = "healthier_food_" + str(datetime.datetime.now()) Problème de droits d'accès à la base !
+
+# db_name = "healthier_food_" + str(datetime.datetime.now())
+# Problème de droits d'accès à la base !
 db_name = "healthier_food"
 
 # database = Database(f'mysql+pymysql://{mysql_id}:{mysql_pw}@localhost')
-database = Database(f'mysql+pymysql://{mysql_id}:{mysql_pw}@localhost/{db_name}?charset=utf8')
+database = Database(f'''mysql+pymysql://{mysql_id}:{mysql_pw}@localhost/
+    {db_name}?charset=utf8''')
 
 nutrition_grades = ["a", "b", "d", "e"]
 
-categories = ["Boissons sucrées", "Pains", "Biscuits", "Desserts", "Céréales au chocolat"]
+tag_categories = ["Sodas", "Viennoiseries", "Biscuits secs", "Crèmes glacées",
+                  "Céréales au chocolat", "Pizzas"]
