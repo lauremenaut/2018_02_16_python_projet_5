c864a26bb8d691ea755a8d0fb08e4db512c77c26
Laure <lauremenaut@yahoo.fr>
2018-03-19 14:19:31 -0400

    Added categories - 'Uppered' constantes names - Added comments

--- config.py
+++ config.py
@@ -7,18 +7,77 @@ from records import Database
 
 import datetime
 
-from untracked_config import mysql_id, mysql_pw
+from untracked_config import MYSQL_ID, MYSQL_PW
 
 
 # db_name = "healthier_food_" + str(datetime.datetime.now())
 # Problème de droits d'accès à la base !
 db_name = "healthier_food"
 
-# database = Database(f'mysql+pymysql://{mysql_id}:{mysql_pw}@localhost')
-database = Database(f'''mysql+pymysql://{mysql_id}:{mysql_pw}@localhost/
-    {db_name}?charset=utf8''')
+# database = Database(f'mysql+pymysql://{MYSQL_ID}:{MYSQL_PW}@localhost')
+database = Database(f'''mysql+pymysql://{MYSQL_ID}:\
+{MYSQL_PW}@localhost/{db_name}?charset=utf8''')
 
+# Tags for nutrition_grades of healthy and unhealthy products
 nutrition_grades = ["a", "b", "d", "e"]
 
-tag_categories = ["Sodas", "Viennoiseries", "Biscuits secs", "Crèmes glacées",
-                  "Céréales au chocolat", "Pizzas"]
+# Tags for (unhealthy) products categories
+tag_categories = ["Pizzas",
+                  "Céréales pour petit-déjeuner",
+                  "Snacks sucrés",
+                  "Confiseries",
+                  "Sodas",
+                  "Plats préparés",
+                  "Produits laitiers",
+                  "Produits à tartiner",
+                  "Crèmes glacées",
+                  "Fromages"
+                  ]
+"""
+
+Requête pour récupérer les ids des catégories qui contiennent au moins 10 produits sains :
+
+SELECT Product_Categorie.categorie_id, COUNT(*) AS nb_products
+FROM Product
+JOIN Product_Categorie
+ON Product_Categorie.product_id = Product.product_id
+WHERE Product.nutriscore = "a" OR Product.nutriscore = "b"
+GROUP BY Product_Categorie.categorie_id
+HAVING nb_products > 10
+ORDER BY nb_products;
+
+
+Requête pour récupérer le nom de des categories correspondant à ces ids:
+
+SELECT name
+FROM Categorie
+WHERE categorie_id IN (47, 48, 798, 31, 1057, 1220, 997, 24, 811, 922);
+WHERE categorie_id IN (2, 44, 1007, 826, 55, 45, 49, 1014, 46, 56);
+
+WHERE categorie_id IN ();
+WHERE categorie_id IN ();
+
+WHERE categorie_id IN (1,27, 23, 22, 26, 25, 29, 28, 31);
+WHERE categorie_id IN (635, 33, 30, 633, 632, 36, 34, 1102, 38, 37);
+WHERE categorie_id IN (2, 35, 1106, 591, 1104, 592, 1103, 40, 39);
+
+
+SELECT Product.product_id
+FROM Product
+WHERE Product.name = "Ferme et fondant, chocolat noir (0,8 % mg) 4 pots";
+
+SELECT store_id
+FROM Product_Store
+WHERE product_id = (SELECT Product.product_id
+FROM Product
+WHERE Product.name = "Ferme et fondant, chocolat noir (0,8 % mg) 4 pots");
+
+SELECT Store.name
+FROM Store
+WHERE store_id = (SELECT store_id
+FROM Product_Store
+WHERE product_id = (SELECT Product.product_id
+FROM Product
+WHERE Product.name = "Ferme et fondant, chocolat noir (0,8 % mg) 4 pots"));
+
+"""
