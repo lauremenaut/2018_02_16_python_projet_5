7745abf8261002354cd602c35890abbcee2c3cbc
Laure <lauremenaut@yahoo.fr>
2018-03-27 16:41:26 -0400

    store.py : Renamed insert_store() & select_store() methods as insert() & select()

--- store.py
+++ store.py
@@ -13,8 +13,9 @@ from config import database
 class Store:
     """ Sets Store class.
 
-    Class consists of 2 methods :
+    Class consists of 3 methods :
         - __init__()
+        - insert()
         - select_store()
 
     """
@@ -22,14 +23,14 @@ class Store:
         """ Store constructor """
         pass
 
-    def insert_store(self, store):
+    def insert(self, store):
         # Store information is added in Store and
         # Product_Store tables (Unique Key on store name column
         # prevents duplicate entry)
             database.query('''INSERT IGNORE INTO Store (name)
                            VALUES (:store)''', store=store)
 
-    def select_store(self, store_id):
+    def select(self, store_id):
         store = database.query('''SELECT Store.name
                                FROM Store
                                WHERE Store.store_id = :store_id''',
