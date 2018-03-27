7bccde704b775c29152e15c49d52d277ccd74828
Laure <lauremenaut@yahoo.fr>
2018-03-26 23:09:33 -0400

    product_categorie.py : Renamed select_product_categories_id(self, code) as select_categories_id_based_on_product_id(self, product_id) - Combined select_unhealthy_product_categories_id() & select_healthy_product_categories_id() as select_categories_id_based_on_product_name() method - Added delete_line() method

--- product_categorie.py
+++ product_categorie.py
@@ -23,38 +23,25 @@ class Product_Categorie:
         """ Product_Categorie constructor """
         pass
 
-    def select_product_categories_id(self, code):
+    def select_categories_id_based_on_product_id(self, product_id):
+        categories_id = \
+            database.query('''SELECT Product_Categorie.categorie_id
+                           FROM Product_Categorie
+                           WHERE Product_Categorie.product_id = :product_id''',
+                           product_id=product_id)
+        return categories_id
+
+    def select_categories_id_based_on_product_name(self, product_name):
+        # Retrieves id of categories to which belongs the chosen
+        # unhealthy product
         categories_id = \
             database.query('''SELECT Product_Categorie.categorie_id
                            FROM Product_Categorie
                            JOIN Product
                            ON Product.product_id = Product_Categorie.product_id
-                           WHERE Product.product_id = :code''',
-                           code=code)
+                           WHERE Product.name = :name''',
+                           name=product_name)
         return categories_id
-
-    def select_unhealthy_product_categories_id(self, unhealthy_product):
-        # Retrieves id of categories to which belongs the chosen
-        # unhealthy product
-        unhealthy_product_categories_id = \
-            database.query('''SELECT Product_Categorie.categorie_id
-                           FROM Product_Categorie
-                           JOIN Product
-                           ON Product.product_id = Product_Categorie.product_id
-                           WHERE Product.name = :name''',
-                           name=unhealthy_product)
-        return unhealthy_product_categories_id
-
-    def select_healthy_product_categories_id(self, healthy_product_name):
-        healthy_product_categories_ids = \
-            database.query('''SELECT Product_Categorie.categorie_id
-                           FROM Product_Categorie
-                           JOIN Product
-                           ON Product.product_id = \
-                               Product_Categorie.product_id
-                           WHERE Product.name = :name''',
-                           name=healthy_product_name)
-        return healthy_product_categories_ids
 
     def delete_line(self, product_id, categorie_id):
         database.query('''DELETE FROM Product_Categorie
