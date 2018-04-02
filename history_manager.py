ce9de98474b9ab40f53e0510686ca44a6c62f7dc
Laure <lauremenaut@yahoo.fr>
2018-04-01 22:31:34 -0400

    history_manager.py : Renamed history.py - Deleted __init__() method - Added docstrings - Corrected 'phyton' into 'python' in line 1

--- history_manager.py
+++ history_manager.py
@@ -1,29 +1,28 @@
-#! /usr/bin/env phyton3
+#! /usr/bin/env python3
 # coding: utf-8
 
-""" Sets History class.
+""" Sets HistoryManager class.
 
-History class ...
+HistoryManager class ...
 
 """
 
 from config import database
 
 
-class History:
-    """ Sets History class.
+class HistoryManager:
+    """ Sets HistoryManager class.
 
-    Class consists of 3 methods :
-        - __init__()
+    Class consists of 2 methods :
         - insert()
         - select()
 
     """
-    def __init__(self):
-        """ History constructor """
-        pass
-
     def insert(self, unhealthy_product, name, description, stores, url):
+        """ Adds query information (= unhealthy and healthy product
+        information) into History table
+
+        """
         database.query('''INSERT INTO History
                        VALUES (NULL,
                                NOW(),
@@ -39,6 +38,10 @@ class History:
                        url=url)
 
     def select(self):
+        """ Returns selected query information (= unhealthy and healthy
+        product information) from History table
+
+        """
         last_results = database.query('''SELECT *
                                        FROM History
                                        ORDER BY request_date DESC
