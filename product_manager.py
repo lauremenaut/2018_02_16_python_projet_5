7742503262e9092a03d3914b5be89b6101413c0d
Laure <lauremenaut@yahoo.fr>
2018-04-01 22:34:33 -0400

    product_manager.py : Renamed product.py - Deleted __init__() method - Added docstrings - Corrected 'phyton' into 'python' in line 1

--- product_manager.py
+++ product_manager.py
@@ -1,20 +1,19 @@
-#! /usr/bin/env phyton3
+#! /usr/bin/env python3
 # coding: utf-8
 
-""" Sets Product class.
+""" Sets ProductManager class.
 
-Product class ...
+ProductManager class ...
 
 """
 
 from config import database
 
 
-class Product:
-    """ Sets Product class.
+class ProductManager:
+    """ Sets ProductManager class.
 
-    Consists of 10 methods :
-        - __init__()
+    Consists of 9 methods :
         - insert()
         - select_product_information()
         - select_products_information()
@@ -24,14 +23,14 @@ class Product:
         - update_description()
         - update_brand()
         - update_nutrition_grade()
+
     """
-    def __init__(self):
-        """ Product constructor """
-        pass
 
     def insert(self, code, name, description, brand, url, nutrition_grade):
-        # Product information is added in Product table
-        # 'code' is saved in 'product_id' column
+        """ Adds product information into Product table
+        Note : 'code' is saved as 'product_id'
+
+        """
         database.query('''INSERT IGNORE INTO Product
                VALUES (:code,
                        :name,
@@ -47,8 +46,7 @@ class Product:
                        nutrition_grade=nutrition_grade)
 
     def select_product_information(self, code):
-        ''' Selects product information for product which product_id is
-        given as a parameter (= code) '''
+        """ Returns selected product information for given code (/product_id) """
         product_information = \
             database.query('''SELECT Product.product_id,
                                      Product.name,
@@ -61,6 +59,10 @@ class Product:
         return product_information
 
     def select_products_information(self, categorie, n_g_1, n_g_2):
+        """ Returns selected products information for given categorie & nutrition
+        grades
+
+        """
         products_information = \
             database.query('''SELECT Product.product_id,
                                      Product.name,
@@ -80,19 +82,21 @@ class Product:
                            n_g_2=n_g_2)
         return products_information
 
-# Peut-on combiner ces 2 méthodes ?? Comment mettre n_g en paramètre ...
     def select_match_information(self, name):
-        product_information = \
+        """ Returns selected product name for given product name & nutrition
+        grade = 'a'
+
+        """
+        product_name = \
             database.query('''SELECT Product.name
                            FROM Product
                            WHERE name = :name
                            AND nutrition_grade = "a"''',
                            name=name)
-        return product_information
+        return product_name
 
     def select_healthiest_match_information(self, name):
-        # Retrieves information for the product proposed to the user
-        # ('healthiest_match')
+        """ Returns selected product information for given product name """
         product_information = \
             database.query('''SELECT Product.product_id,
                                      Product.name,
@@ -104,24 +108,28 @@ class Product:
         return product_information
 
     def update_name(self, name, code):
+        """ Updates product name for given product code """
         database.query('''UPDATE IGNORE Product
                        SET name = :name
                        WHERE Product.product_id = :code''',
                        name=name, code=code)
 
     def update_description(self, description, code):
+        """ Updates product description for given product code """
         database.query('''UPDATE IGNORE Product
                        SET description = :description
                        WHERE Product.product_id = :code''',
                        description=description, code=code)
 
     def update_brand(self, brand, code):
+        """ Updates product brand for given product code """
         database.query('''UPDATE IGNORE Product
                        SET brand = :brand
                        WHERE Product.product_id = :code''',
                        brand=brand, code=code)
 
     def update_nutrition_grade(self, nutrition_grade, code):
+        """ Updates product nutrition grade for given product code """
         database.query('''UPDATE IGNORE Product
                        SET nutrition_grade = :nutrition_grade
                        WHERE Product.product_id = :code''',
