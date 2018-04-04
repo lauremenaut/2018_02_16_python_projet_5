6de53b0fed06505947904519038b8384beced406
Laure <lauremenaut@yahoo.fr>
2018-04-04 16:39:49 -0400

    database.py : Modified docstrings

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" Contains configuration data """
+""" Sets 'database' variable, connecting Python script with MySQL.
+Called by tables_creator.py, database_updater.py & all table manager
+files
+
+"""
 
 from records import Database
 
@@ -10,5 +14,5 @@ from params import MYSQL_ID, MYSQL_PW
 with open('db_name.txt', "r") as f:
     db_name = f.read()
 
-database = Database(f'''mysql+pymysql://{MYSQL_ID}:\
-{MYSQL_PW}@localhost/{db_name}?charset=utf8''')
+database = Database(f'''mysql+pymysql://{MYSQL_ID}:{MYSQL_PW}@localhost/\
+{db_name}?charset=utf8''')
