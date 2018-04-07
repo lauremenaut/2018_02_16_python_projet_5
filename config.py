c0b76e689369b73076feeaaddd3d92fb080d7732
Laure <lauremenaut@yahoo.fr>
2018-04-06 22:18:15 -0400

    config.py : Merged database.py & config.py - Added database_connection() method

--- config.py
+++ config.py
@@ -1,7 +1,33 @@
 #! /usr/bin/env python3
 # coding: utf-8
 
-""" Contains tags called by app.py & database_filler.py """
+""" Configuration data.
+
+    Sets database_connection() method.
+    Contains tags for requests criteria.
+
+"""
+
+from records import Database
+
+from params import MYSQL_ID, MYSQL_PW
+
+
+def database_connection():
+    """ Manages connection to the database.
+
+    Opens 'db_name.txt' to retrieve db_name.
+    Connects to MySQL local database via records.Database
+    Returns 'database' object containing connection with database.
+
+    """
+    with open('db_name.txt', "r") as f:
+        db_name = f.read()
+
+    database = Database(f'''mysql+pymysql://{MYSQL_ID}:{MYSQL_PW}@localhost/\
+{db_name}?charset=utf8''')
+
+    return database
 
 # Tags for nutrition grades of healthy and unhealthy products
 nutrition_grades = ["a", "b", "d", "e"]
