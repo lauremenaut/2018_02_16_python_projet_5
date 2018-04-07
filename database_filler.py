7008e74d956835476abae5afb8ec3767a1a3b3c2
Laure <lauremenaut@yahoo.fr>
2018-04-06 22:45:08 -0400

    database_filler.py : Added 'database' as parameter for __init__() method - Created instances for table manager classes - Called 'tables' methods on these instances

--- database_filler.py
+++ database_filler.py
@@ -10,10 +10,10 @@ Open Food Facts API.
 
 from requests import get
 
-from config import nutrition_grades, tag_categories
+from config import database_connection, nutrition_grades, tag_categories
 from product_manager import ProductManager
-from categorie_manager import CategorieManager
-from product_categorie_manager import ProductCategorieManager
+from category_manager import CategoryManager
+from product_category_manager import ProductCategoryManager
 from store_manager import StoreManager
 from product_store_manager import ProductStoreManager
 
@@ -29,20 +29,28 @@ class DatabaseFiller:
 
     """
 
-    def __init__(self):
+    def __init__(self, database):
         """ DatabaseFiller constructor.
 
-        For each categorie and for each nutrition grade, retrieves
+        For each category and for each nutrition grade, retrieves
         corresponding products from Open Food Facts API and adds them to
         local database.
 
         """
+        # self.database = database
+        # Creates instances of table manager classes
+        self.product_manager = ProductManager(database)
+        self.category_manager = CategoryManager(database)
+        self.product_category_manager = ProductCategoryManager(database)
+        self.store_manager = StoreManager(database)
+        self.product_store_manager = ProductStoreManager(database)
+
         for nutrition_grade in nutrition_grades:
-            for categorie in tag_categories:
-                products = self._get_products(categorie, nutrition_grade)
+            for category in tag_categories:
+                products = self._get_products(category, nutrition_grade)
                 self._fill_db(products)
 
-    def _get_products(self, categorie, nutrition_grade):
+    def _get_products(self, category, nutrition_grade):
         """ Return a list of dictionnaries of products information.
 
         Connects to Open Food Facts API via get() method from requests
@@ -59,7 +67,7 @@ class DatabaseFiller:
             'page': 1,
             'tagtype_0': 'categories',
             'tag_contains_0': 'contains',
-            'tag_0': categorie,
+            'tag_0': category,
             'tagtype_1': 'nutrition_grades',
             'tag_contains_1': 'contains',
             'tag_1': nutrition_grade
@@ -91,8 +99,8 @@ class DatabaseFiller:
                 self.url = product['url']
                 categories_to_strip = (product['categories']).split(',')
                 self.categories = []
-                for categorie in categories_to_strip:
-                    self.categories.append(categorie.strip().capitalize())
+                for category in categories_to_strip:
+                    self.categories.append(category.strip().capitalize())
                 stores_to_strip = (product['stores']).split(',')
                 self.stores = []
                 for store in stores_to_strip:
@@ -105,19 +113,20 @@ class DatabaseFiller:
             if all([self.code, self.name, self.description, self.brand,
                     self.url, self.nutrition_grade, self.categories[0],
                     self.stores[0]]):
-                ProductManager.insert(self, self.code, self.name,
-                                      self.description, self.brand,
-                                      self.url, self.nutrition_grade)
-                for categorie in self.categories:
-                    CategorieManager.insert(self, categorie)
-                    ProductCategorieManager.insert(self, categorie, self.name)
+                self.product_manager.insert(self.code, self.name,
+                                            self.description, self.brand,
+                                            self.url, self.nutrition_grade)
+                for category in self.categories:
+                    self.category_manager.insert(category)
+                    self.product_category_manager.insert(category, self.name)
                 for store in self.stores:
-                    StoreManager.insert(self, store)
-                    ProductStoreManager.insert(self, store, self.name)
+                    self.store_manager.insert(store)
+                    self.product_store_manager.insert(store, self.name)
+
 
 def main():
-    DatabaseFiller()
-
+    database = database_connection()
+    DatabaseFiller(database)
 
 if __name__ == '__main__':
     main()
